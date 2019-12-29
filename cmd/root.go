29c7d18be105855f33413973b9e8462b5a6436b7
Aleksandar Micic <tfrs1@users.noreply.github.com>
2019-12-29 15:45:00 +0100

    Add version information

--- cmd/root.go
+++ cmd/root.go
@@ -7,12 +7,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "prctl",
 	Short: "prctl is something awesome",
 	Long: `A Fast and Flexible Static Site Generator built with
 				  love by spf13 and friends in Go.
 				  Complete documentation is available at http://hugo.spf13.com`,
+	Version: fmt.Sprintf("%v, commit %v, built at %v", version, commit, date),
 	Run: func(cmd *cobra.Command, args []string) {
 	},
 }
